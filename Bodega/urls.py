c4ae96af0964940bc9fe5f93a3ca627036c0d3bf
Gustavo Roque <gustavo.roque.143@gmail.com>
2023-12-14 15:13:40 -0600

    Adding Endpoitns

--- Bodega/urls.py
+++ Bodega/urls.py
@@ -1,13 +1,29 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    #Get all the items
     path('category/',views.getAllCategories),
-    path('detarticles/',views.getAllDetArticles),
-    path('genarticles/',views.getAllGenArticles),
+    path('detail_article/',views.getAllDetArticles),
+    path('general_article/',views.getAllGenArticles),
     path('loan/',views.getAllLoans),
+    #Create
+    path('category/create',views.CreateCategory),
+    path('detail_article/create',views.CreateDetailArticle),
+    path('general_article/create',views.CreateGeneralArticle),
+    path('loan/create',views.CreateLoan),
 
+    #View specific register 
     path('category/<int:catid>', views.getCategory),
-    path('detarticle/<int:detartid>', views.getGenArticle),
-    path('genarticles/<int:genartid>', views.getDetArticle),
-    path('loan/<int:loanid>', views.getLoan)
+    path('detail_article/<int:detail_article_ID>', views.getDetArticle),
+    path('general_article/<int:general_article_ID>', views.getGenArticle),
+    path('loan/<int:loanid>', views.getLoan),
+
+    #Update register
+    path('category/<int:category_ID>/update', views.updateCategory),
+    path('general_article/<int:general_article_ID>/update', views.updateGeneralArticle),
+    path('detail_article/<int:detail_article_ID>/update', views.updateDetailArticle),
+    path('loan/<int:Loan_ID>/update', views.updateLoan)
+
+    #Delete Registers
+
 ]
--- Bodega/views.py
+++ Bodega/views.py
@@ -5,96 +5,159 @@ from .models import Category, GeneralArticle, DetailArticle, Loan
 from .serializers import CategorySerializers, GeneralArticleSerializers,DetailArticleSerializer,LoanSerializers
 
 
-@api_view(['GET','POST'])
+#Get Method
+@api_view(['GET'])
 def getAllCategories(request):
-    if request.method == 'GET':
-        
-        categories = Category.objects.all()
-        serializer = CategorySerializers(categories, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        newCatSer = CategorySerializers(data=request.data)
-        if newCatSer.is_valid():
-            newCatSer.save()
-            return Response(newCatSer.data)
-        return Response(newCatSer.errors)
-        
 
-@api_view(['GET','POST'])
+    categories = Category.objects.all()
+    serializer = CategorySerializers(categories, many=True)
+    return Response(serializer.data)        
+
+@api_view(['GET'])
 def getAllDetArticles(request):
-    if request.method == 'GET':
-        detArticles = DetailArticle.objects.all()
-        serializer = DetailArticleSerializer(detArticles, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        newDetArticle = DetailArticleSerializer(data = request.data)
-        if newDetArticle.is_valid():
-            newDetArticle.save()
-            return Response(newDetArticle.data)
-        return Response(newDetArticle.errors)
+    detArticles = DetailArticle.objects.all()
+    serializer = DetailArticleSerializer(detArticles, many=True)
+    return Response(serializer.data)
 
-@api_view(['GET','POST'])
+@api_view(['GET'])
 def getAllGenArticles(request):
-    if request.method == 'GET':
-        genArticles = GeneralArticle.objects.all()
-        serializer = GeneralArticleSerializers(genArticles, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        newGenArticle = GeneralArticleSerializers(data = request.data)
-        if newGenArticle.is_valid():
-            newGenArticle.save()
-            return Response(newGenArticle.data)
-        return Response(newGenArticle.errors)
+    genArticles = GeneralArticle.objects.all()
+    serializer = GeneralArticleSerializers(genArticles, many=True)
+    return Response(serializer.data)
     
-@api_view(['GET','POST'])
+@api_view(['GET'])
 def getAllLoans(request):
-    if request.method == 'GET':
-        loan = Loan.objects.all()
-        serializer = LoanSerializers(loan, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        newLoan = LoanSerializers(data = request.data)
-        if newLoan.is_valid():
-            newLoan.save()
-            return Response(newLoan.data)
-        return Response(newLoan.errors)
+    loan = Loan.objects.all()
+    serializer = LoanSerializers(loan, many=True)
+    return Response(serializer.data)
 
-@api_view(['GET','PATCH','DELETE'])
+#Create 
+@api_view(['POST'])
+def CreateCategory(request):
+    newCategory = CategorySerializers(data = request.data)
+    if newCategory.is_valid():
+        newCategory.save()
+        return Response(newCategory.data)
+    return Response(newCategory.errors)
+
+@api_view(['POST'])
+def CreateGeneralArticle(request):
+    newGeneral_Article = GeneralArticleSerializers(data = request.data)
+    if newGeneral_Article.is_valid():
+        newGeneral_Article.save()
+        return Response(newGeneral_Article.data)
+    return Response(newGeneral_Article.errors)
+
+@api_view(['POST'])
+def CreateDetailArticle(request):
+    newDetail_Article = DetailArticleSerializer(data = request.data)
+    if newDetail_Article.is_valid():
+        newDetail_Article.save()
+        return Response(newDetail_Article.data)
+    return Response(newDetail_Article.errors)
+
+@api_view(['POST'])
+def CreateLoan(request):
+    newLoan = LoanSerializers(data = request.data)
+    if newLoan.is_valid():
+        newLoan.save()
+        return Response(newLoan.data)
+    return Response(newLoan.errors)
+
+#Get one item
+@api_view(['GET'])
 def getCategory(request,catid):
     try:
         category = Category.objects.get(CatID=catid)
     except category.DoesNotExist:
         return Response({'error': 'Item not found'})
     
-    if request.method == 'GET':
-        serializers = CategorySerializers(category, many=False)
+    serializers = CategorySerializers(category, many=False)
 
     # category = Category.objects.get(CatID=catid)
-        return Response(serializers.data)
-    elif request.method == 'PATCH':
-        serializers = CategorySerializers(category,data=request.data,partial= True)
-
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data)
-        return  Response(serializers.errors)
+    return Response(serializers.data)
+    
+        
     
 @api_view(['GET'])
-def getGenArticle(request,detartid):
-    category = Category.objects.get(CatID=detartid)
-    serializers = CategorySerializers(category, many=False)
+def getGenArticle(request,general_article_ID):
+    try:
+        general_article = GeneralArticle.objects.get(GenArticleID=general_article_ID)
+    except:
+        return Response({'error':'Item Not Found'})
+    
+    serializers = GeneralArticleSerializers(general_article, many=False)
     return Response(serializers.data)
 
 @api_view(['GET'])
-def getDetArticle(request,genartid):
-    uniqueDetArt = DetailArticle.objects.get(DetailArticle=genartid)
-    serializers = CategorySerializers(uniqueDetArt, many=False)
+def getDetArticle(request,detail_article_ID):
+    try:
+
+        uniqueDetArt = DetailArticle.objects.get(DetArticleID=detail_article_ID)
+    except:
+        return Response({'error':'Not Found'})
+    serializers = DetailArticleSerializer(uniqueDetArt, many=False)
     return Response(serializers.data)
 
 @api_view(['GET'])
 def getLoan(request,loanid):
-    loan = Loan.objects.get(loanID=loanid)
+    try:
+        loan = Loan.objects.get(LoanID=loanid)
+    except:
+        return Response({'error':'Item not Found'})
     serializers = LoanSerializers(loan, many=False)
     return Response(serializers.data)
 
 
+#Update one reg
+@api_view(['PATCH'])
+def updateCategory(request,category_ID):
+    try:
+        category = Category.objects.get(CatID=category_ID)
+    except category.DoesNotExist:
+        return Response({'error': 'Item not found'})
+    serializers = CategorySerializers(category,data=request.data,partial= True)
+    
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data)
+    return  Response(serializers.errors)
+
+@api_view(['PATCH'])
+def updateGeneralArticle(request,general_article_ID):
+    try:
+        general_Article = GeneralArticle.objects.get(GenArticleID=general_article_ID)
+    except general_Article.DoesNotExist:
+        return Response({'error': 'Item not found'})
+    serializers = GeneralArticleSerializers(general_Article,data=request.data,partial= True)
+    
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data)
+    return  Response(serializers.errors)
+
+@api_view(['PATCH'])
+def updateDetailArticle(request,detail_article_ID):
+    try:
+        detail_Article = DetailArticle.objects.get(DetArticleID=detail_article_ID)
+    except detail_Article.DoesNotExist:
+        return Response({'error': 'Item not found'})
+    serializers = DetailArticleSerializer(detail_Article,data=request.data,partial= True)
+    
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data)
+    return  Response(serializers.errors)
+
+@api_view(['PATCH'])
+def updateLoan(request,Loan_ID):
+    try:
+        loan = Loan.objects.get(LoanID=Loan_ID)
+    except loan.DoesNotExist:
+        return Response({'error': 'Item not found'})
+    serializers = LoanSerializers(loan,data=request.data,partial= True)
+    
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data)
+    return  Response(serializers.errors)
